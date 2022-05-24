a3d94f45afd435ef277b98554d48d1e94bfad150
alex roel <you@example.com>
2022-05-24 13:31:28 -0500

    js-ex

--- src/user.handler.js
+++ src/user.handler.js
@@ -1,48 +1,46 @@
 const Users = require('./user.model')
 
 const User = {
-    
-    list: async (req, res)=>{
+    list: async (req, res) => {
         const users = await Users.find()
         res.status(200).send(users)
     },
 
-    create: async (req, res) => {
-        //console.log(req.body)
+    create: async (req, res) =>{
         const user = new Users(req.body)
         const saveUser = await user.save()
         res.status(201).send(saveUser._id)
     },
 
-    get: async (req, res) => {
+    get: async (req, res) =>{
         const { id } = req.params
         const user = await Users.findOne({_id: id})
-        res.status(200).send( user )
+        res.status(200).send( user)
     },
 
     update: async (req, res) =>{
-        //Optener el usuario mediante ID 
+        //Obtener user por Id
         const { id } = req.params
         const user = await Users.findOne({_id: id})
 
-        //Actualizar 
+        //Actualizar un dato
         Object.assign(user, req.body)
         await user.save()
         res.sendStatus(204)
     },
 
     delete: async (req, res) =>{
-        //Optener el usuario mediante ID 
+        //Obtener un user Id
         const { id } = req.params
         const user = await Users.findOne({_id: id})
 
-        //Eleminar  
+        //Eliminar un dato
         if(user){
             user.remove()
         }
 
         res.sendStatus(204)
-    }
+    },
 }
 
 module.exports = User
